1ff43cf42269a66dbd11ba993651e61d1ec06593
David Beltran <davidalfonsobeltran5@gmail.com>
2020-09-30 16:58:18 -0500

    clase

---
+++ Senal.m
@@ -0,0 +1,96 @@
+classdef Senal
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        seno
+        cua
+        tri
+        pul
+        exp
+        ramp1
+        ramp2 
+        ramp3
+    end
+    
+    methods
+        function obj = Senal()
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            
+            
+            
+        end
+        
+        function obj = Seno(obj,F,A)
+            t=0:2/(100*F):2/F;
+            obj.seno=[t ; A*sin(2*pi*F*t)];
+        end
+        function obj = Cuadra(obj,F,A)
+            t=0:2/(100*F):2/F;
+            obj.seno=[t ; A*square(2*pi*F*t)];
+        end
+        function obj = Trian(obj,F,A)
+            t=0:2/(100*F):2/F;
+            obj.seno=[t ; A*sawtooth((2*pi*F*t)+(pi/2),0.5)];
+        end
+        
+        function obj= Expo(obj,a,b,ta,tb)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here  
+            
+            t=ta:(tb-ta)/100:tb;
+            y=a*exp(-b*t);
+            obj.exp=[t;y];
+        end
+        
+        function obj=Rampa1(obj,ta,tb)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            tq=tb-ta;
+            t= ta:(tb-ta)/100:tb;
+            t1= ta:(tb-ta)/100:ta+0.3*tq;
+            t2= ta+0.3*tq+(tb-ta)/100:(tb-ta)/100:ta+0.7*tq;
+            t3= ta+0.7*tq+(tb-ta)/100:(tb-ta)/100:tb;
+            y1= zeros(1,length(t1));
+            y2= 0.825*t2-0.825*(ta+0.3*tq+(tb-ta)/100);
+            y3= y2(length(y2))*ones(1,length(t3)); 
+            y=[y1,y2,y3];
+            obj.ramp1=[t;y];
+        end
+        
+        function obj= Rampa2(obj,ta,tb)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here 
+            tq=tb-ta;
+            t= ta:(tb-ta)/100:tb;
+            t1= ta:(tb-ta)/100:ta+0.3*tq;
+            t2= ta+0.3*tq+(tb-ta)/100:(tb-ta)/100:ta+0.7*tq;
+            t3= ta+0.7*tq+(tb-ta)/100:(tb-ta)/100:tb;
+            
+            y2= -0.825*t2+0.825*(ta+0.7*tq+(tb-ta)/100);
+            y1= y2(1)*ones(1,length(t1));
+            y3= zeros(1,length(t3)); 
+            y=[y1,y2,y3];
+            obj.ramp2=[t;y];
+        end
+        
+        function obj= Rampa3(obj,ta,tb)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here 
+            tq=tb-ta;
+            t= ta:(tb-ta)/100:tb;
+            t1= ta:(tb-ta)/100:ta+0.3*tq;
+            t2= ta+0.3*tq+(tb-ta)/100:(tb-ta)/100:ta+0.7*tq;
+            t3= ta+0.7*tq+(tb-ta)/100:(tb-ta)/100:tb;
+            y1= 1.1*t1-1.1*ta;
+            y2= y1(length(y1))*ones(1,length(t2));
+            y3= -1.1*t3+1.1*tb;
+            y=[y1,y2,y3];
+            obj.ramp3=[t;y];
+            
+        end
+        
+    end
+end
+
